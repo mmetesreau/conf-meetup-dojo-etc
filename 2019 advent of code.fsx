d860998a0e091e92aa922dadef1f0b393c3b558c
Mickael Metesreau <mickael.metesreau@gmail.com>
2019-12-05 11:56:02 +0100

    Stuff

--- 2019 advent of code.fsx
+++ 2019 advent of code.fsx
@@ -328,3 +328,186 @@ module Day4 =
         |> List.filter(fun password -> password |> neverDecrease)
         |> List.filter(fun password -> password |> containsDouble')
         |> List.length
+
+// --- Day 5: Sunny with a Chance of Asteroids ---
+
+//module Day5
+type Opcodes = 
+        | Add of int * int * int
+        | Multiply of int * int * int
+        | Print of int
+        | Write of int 
+        | JumpIfTrue of int * int
+        | JumpIfFalse of int * int
+        | LessThan of int * int * int
+        | Equals of int * int * int
+        | Halt
+
+let offset = function
+    | Add _ ->  4
+    | Multiply _ -> 4
+    | Write _ ->  2
+    | Print _ ->  2
+    | JumpIfTrue _ -> 3
+    | JumpIfFalse _ -> 3
+    | LessThan _ -> 4
+    | Equals _ -> 4
+    | Halt -> 0
+
+type Modes = Immediate | Position
+
+let mode (mode: char option) = 
+    match mode with
+    | Some '1' -> Immediate
+    | _ -> Position
+
+let parameter (memory: int array) (mode: Modes) (parameterPointer: int) =
+    let parameter = memory.[parameterPointer]
+    match mode with
+    | Position -> memory.[parameter]
+    | _ -> parameter
+
+let instruction (entry: int) =
+    let instruction = string entry 
+    if instruction.Length <= 2 then
+        Seq.empty , int instruction
+    else
+        instruction.Substring(0,instruction.Length - 2) |> Seq.rev, instruction.Substring(instruction.Length - 2) |> int
+
+let (|AddInstruction|_|) (parameterMode, opcode) =
+    if opcode = 1 then
+         Some (parameterMode |> Seq.tryItem 0 |> mode, parameterMode |> Seq.tryItem 1 |> mode, Immediate)
+    else None     
+
+let (|MultiplyInstruction|_|) (parameterMode, opcode) =
+    if opcode = 2 then
+         Some (parameterMode |> Seq.tryItem 0 |> mode, parameterMode |> Seq.tryItem 1 |> mode, Immediate)
+    else None            
+
+let (|WriteInstruction|_|) (_, opcode) =
+    if opcode = 3 then
+         Some Immediate
+    else None 
+
+let (|PrintInstruction|_|) (parameterMode, opcode) =
+    if opcode = 4 then
+         Some  (parameterMode |> Seq.tryItem 0 |> mode)
+    else None 
+
+let (|JumpIfTrueInstruction|_|) (parameterMode, opcode) =
+    if opcode = 5 then
+         Some (parameterMode |> Seq.tryItem 0 |> mode, parameterMode |> Seq.tryItem 1 |> mode)
+    else None 
+
+let (|JumpIfFalseInstruction|_|) (parameterMode, opcode) =
+    if opcode = 6 then
+         Some (parameterMode |> Seq.tryItem 0 |> mode, parameterMode |> Seq.tryItem 1 |> mode)
+    else None 
+
+let (|LessThanInstruction|_|) (parameterMode, opcode) =
+    if opcode = 7 then
+         Some (parameterMode |> Seq.tryItem 0 |> mode, parameterMode |> Seq.tryItem 1 |> mode, Immediate)
+    else None 
+
+let (|EqualsInstruction|_|) (parameterMode, opcode) =
+    if opcode = 8 then
+         Some (parameterMode |> Seq.tryItem 0 |> mode, parameterMode |> Seq.tryItem 1 |> mode, Immediate)
+    else None
+
+let (|HaltInstruction|_|) (_, opcode) =
+    if opcode = 99 then
+         Some true
+    else None
+
+let read (memory: int array)  (instructionPointer: int) =
+    match memory.[instructionPointer] |> instruction  with
+    | AddInstruction (pm1, pm2, pm3) ->
+        (parameter memory pm1 (instructionPointer+1), parameter memory pm2 (instructionPointer+2), parameter memory pm3 (instructionPointer+3)) 
+            |> Add
+    | MultiplyInstruction (pm1, pm2, pm3) ->
+        (parameter memory pm1 (instructionPointer+1), parameter memory pm2 (instructionPointer+2), parameter memory pm3 (instructionPointer+3)) 
+            |> Multiply
+    | WriteInstruction pm1 ->
+        parameter memory pm1 (instructionPointer+1) 
+            |> Write
+    | PrintInstruction pm1 ->
+        parameter memory pm1 (instructionPointer+1) 
+            |> Print
+     | JumpIfTrueInstruction (pm1, pm2) ->
+        (parameter memory pm1 (instructionPointer+1), parameter memory pm2 (instructionPointer+2)) 
+            |> JumpIfTrue
+     | JumpIfFalseInstruction (pm1, pm2) ->
+        (parameter memory pm1 (instructionPointer+1), parameter memory pm2 (instructionPointer+2)) 
+            |> JumpIfFalse
+     | LessThanInstruction (pm1, pm2, pm3) ->
+        (parameter memory pm1 (instructionPointer+1), parameter memory pm2 (instructionPointer+2), parameter memory pm3 (instructionPointer+3)) 
+            |> LessThan
+     | EqualsInstruction (pm1, pm2, pm3) ->
+        (parameter memory pm1 (instructionPointer+1), parameter memory pm2 (instructionPointer+2), parameter memory pm3 (instructionPointer+3)) 
+            |> Equals
+    | HaltInstruction _ ->
+        Halt
+    | cmd -> failwith <| sprintf "oups %A" cmd
+
+let execute (memory: int array) =
+    let rec process' (memory: int array) (instructionPointer: int) =
+        let instruction = read memory instructionPointer
+        let step = offset instruction
+        
+        // printfn "instructionPointer %i" instructionPointer
+        // printfn "instruction %A" instruction
+        // printfn "step %i" step
+
+        match instruction with
+        | Halt -> memory
+        | Add (i1, i2, dest) -> 
+            memory.[dest] <- i1 + i2
+            instructionPointer + step |> process' memory 
+        | Multiply (i1, i2, dest) -> 
+            memory.[dest] <- i1 * i2
+            instructionPointer + step |> process' memory
+        | Print i1 -> 
+            printfn "%i" i1
+            instructionPointer + step |> process' memory 
+        | Write i1 -> 
+            memory.[i1] <- Console.ReadLine() |> int
+            instructionPointer + step |> process' memory 
+        | JumpIfTrue (i1, i2) -> 
+            printfn "jumpiftrue %i %i" i1 i2
+            if i1 = 0 then
+                instructionPointer + step |> process' memory 
+            else
+                i2 |> process' memory 
+        | JumpIfFalse (i1, i2) -> 
+            printfn "jumpiffalse %i %i" i1 i2
+
+            if i1 = 0 then
+                i2 |> process' memory 
+            else
+                instructionPointer + step |> process' memory 
+        | LessThan  (i1, i2, dest) -> 
+            memory.[dest] <- if i1 < i2 then 1 else 0
+            instructionPointer + step |> process' memory 
+        | Equals (i1, i2, dest) -> 
+            memory.[dest] <- if i1 = i2 then 1 else 0
+            instructionPointer + step |> process' memory                                          
+
+    process' memory 0
+
+let load (program: string) =
+    program
+        |> split ','
+        |> Array.map int
+
+let program = "3,225,1,225,6,6,1100,1,238,225,104,0,1101,72,36,225,1101,87,26,225,2,144,13,224,101,-1872,224,224,4,224,102,8,223,223,1001,224,2,224,1,223,224,223,1102,66,61,225,1102,25,49,224,101,-1225,224,224,4,224,1002,223,8,223,1001,224,5,224,1,223,224,223,1101,35,77,224,101,-112,224,224,4,224,102,8,223,223,1001,224,2,224,1,223,224,223,1002,195,30,224,1001,224,-2550,224,4,224,1002,223,8,223,1001,224,1,224,1,224,223,223,1102,30,44,225,1102,24,21,225,1,170,117,224,101,-46,224,224,4,224,1002,223,8,223,101,5,224,224,1,224,223,223,1102,63,26,225,102,74,114,224,1001,224,-3256,224,4,224,102,8,223,223,1001,224,3,224,1,224,223,223,1101,58,22,225,101,13,17,224,101,-100,224,224,4,224,1002,223,8,223,101,6,224,224,1,224,223,223,1101,85,18,225,1001,44,7,224,101,-68,224,224,4,224,102,8,223,223,1001,224,5,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,7,677,226,224,102,2,223,223,1005,224,329,101,1,223,223,8,677,226,224,1002,223,2,223,1005,224,344,1001,223,1,223,1107,677,677,224,102,2,223,223,1005,224,359,1001,223,1,223,1107,226,677,224,102,2,223,223,1005,224,374,101,1,223,223,7,226,677,224,102,2,223,223,1005,224,389,101,1,223,223,8,226,677,224,1002,223,2,223,1005,224,404,101,1,223,223,1008,226,677,224,1002,223,2,223,1005,224,419,1001,223,1,223,107,677,677,224,102,2,223,223,1005,224,434,101,1,223,223,1108,677,226,224,1002,223,2,223,1006,224,449,101,1,223,223,1108,677,677,224,102,2,223,223,1006,224,464,101,1,223,223,1007,677,226,224,102,2,223,223,1006,224,479,101,1,223,223,1008,226,226,224,102,2,223,223,1006,224,494,101,1,223,223,108,226,226,224,1002,223,2,223,1006,224,509,101,1,223,223,107,226,226,224,102,2,223,223,1006,224,524,101,1,223,223,1107,677,226,224,102,2,223,223,1005,224,539,1001,223,1,223,108,226,677,224,1002,223,2,223,1005,224,554,101,1,223,223,1007,226,226,224,102,2,223,223,1005,224,569,101,1,223,223,8,226,226,224,102,2,223,223,1006,224,584,101,1,223,223,1008,677,677,224,1002,223,2,223,1005,224,599,1001,223,1,223,107,226,677,224,1002,223,2,223,1005,224,614,1001,223,1,223,1108,226,677,224,102,2,223,223,1006,224,629,101,1,223,223,7,677,677,224,1002,223,2,223,1005,224,644,1001,223,1,223,108,677,677,224,102,2,223,223,1005,224,659,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,674,101,1,223,223,4,223,99,226"
+
+// program 
+//     |> load
+//     |> execute
+ 
+ // --- Part Two ---
+
+
+"3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
+    |> load 
+    |> execute
